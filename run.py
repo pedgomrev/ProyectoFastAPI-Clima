6f0132fdeadbf84f99e590662454b281970b6efc
Pedro Gómez Revilla <pedrogomezrevilla@gmail.com>
2024-08-23 12:45:52 +0200

    Cambios deploy render

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from app.main import app
 import uvicorn
+import os
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))  # Usa el puerto de Render o 8000 por defecto
+    uvicorn.run(app, host="0.0.0.0", port=port)
